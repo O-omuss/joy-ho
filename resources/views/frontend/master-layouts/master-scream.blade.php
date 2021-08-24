913bca264646ed55b481e89c5d81deac8925347b
O-omuss <patramustu@gmail.com>
2021-08-24 12:29:15 +0530

    changes

--- resources/views/frontend/master-layouts/master-scream.blade.php
+++ resources/views/frontend/master-layouts/master-scream.blade.php
@@ -6,27 +6,23 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>Joyville Homes - Affordable Housing Project in Gurgaon, Howrah, Virar, Hinjewadi, Pune & Manjri</title>
-        <meta name="description"
-            content="Stay connected with our media coverage and news section for our recent achievements, success stories, interviews from our management team, etc." />
-        <meta name="keywords"
-            content="Residential property News,real estate news,Indian housing market news,upcoming Residential Projects in Kolkata,SP real estate launch news,New projects in Howrah" />
+        <meta name="description" content="Stay connected with our media coverage and news section for our recent achievements, success stories, interviews from our management team, etc." />
+        <meta name="keywords" content="Residential property News,real estate news,Indian housing market news,upcoming Residential Projects in Kolkata,SP real estate launch news,New projects in Howrah" />
         <meta property="og:type" content="website">
-        <meta property="og:title"
-            content="Joyville Homes - Affordable Housing Project in Gurgaon, Howrah, Virar, Hinjewadi, Pune & Manjri">
+        <meta property="og:title" content="Joyville Homes - Affordable Housing Project in Gurgaon, Howrah, Virar, Hinjewadi, Pune & Manjri">
         <meta property="og:site_name" content="Joyville Homes">
-        <meta property="og:url" content="https://www.joyvillehomes.com/">
-        <meta property="og:description"
-            content="Explore Joyville Homes by Shapoorji Pallonji Affordable Housing Project in Gurgaon, Howrah, Virar, Hinjewadi, Pune, Manjri. Book your dream home now!">
-        <meta property="og:image" content="https://www.joyvillehomes.com/assets/vectors/icon_joyvillelogo_color.svg">
+        <meta property="og:url" content="https://joyho.joyvillehomes.com">
+        <meta property="og:description" content="Explore Joyville Homes by Shapoorji Pallonji Affordable Housing Project in Gurgaon, Howrah, Virar, Hinjewadi, Pune, Manjri. Book your dream home now!">
+        {{-- <meta property="og:image" content="https://www.joyvillehomes.com/assets/vectors/icon_joyvillelogo_color.svg"> --}}
+        <meta property="og:image" content="{{asset('dist-scream/images/joyville-logo.png')}}">
         <meta name="twitter:card" content="summary">
         <meta name="twitter:site" content="@JoyvilleHomes">
-        <meta name="twitter:title"
-            content="Joyville Homes - Affordable Housing Project in Gurgaon, Howrah, Virar, Hinjewadi, Pune & Manjri" />
-        <meta name="twitter:description"
-            content="Explore Joyville Homes by Shapoorji Pallonji Affordable Housing Project in Gurgaon, Howrah, Virar, Hinjewadi, Pune, Manjri. Book your dream home now!">
-        <meta name="twitter:image" content="https://www.joyvillehomes.com/assets/vectors/icon_joyvillelogo_color.svg" />
+        <meta name="twitter:title" content="Joyville Homes - Affordable Housing Project in Gurgaon, Howrah, Virar, Hinjewadi, Pune & Manjri" />
+        <meta name="twitter:description" content="Explore Joyville Homes by Shapoorji Pallonji Affordable Housing Project in Gurgaon, Howrah, Virar, Hinjewadi, Pune, Manjri. Book your dream home now!">
+        {{-- <meta name="twitter:image" content="https://www.joyvillehomes.com/assets/vectors/icon_joyvillelogo_color.svg" /> --}}
+        <meta name="twitter:image" content="{{asset('dist-scream/images/joyville-logo.png')}}" />
         <link rel="icon" type="image/x-icon" href="https://www.joyvillehomes.com/favicon.ico?v13feb2019">
-        <link rel="canonical" href="https://www.joyvillehomes.com" />
+        <link rel="canonical" href="https://joyho.joyvillehomes.com" />
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
             integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
